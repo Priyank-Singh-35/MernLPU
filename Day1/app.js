2d79bcd4082e3e06e13f5a16186d4ba481932f1d
Priyank Singh <priyanksingh5004@gmail.com>
2025-06-16 13:42:43 +0530

    first commit

---
+++ Day1/app.js
@@ -0,0 +1,133 @@
+//Notes:-
+
+//cases                          var             let             const
+//access before initialisation   yes             ---             ----
+//re-declaration                 yes             ---             ----
+//re-assignment                  yes             yes             ----
+
+//--------------------------------
+
+
+//window.console.log("Hello from JS!");
+//-------------------------------
+
+//let,const OR var
+
+//let age=30;
+//const  year=2010;
+//var block=22;
+
+//console.log(age, year, block);
+//-------------------------------
+
+// console.log(age);
+
+// var age=21;      // declaration & initialization
+// console.log(age);
+
+// var age=42;     // re-declaration
+// console.log(age);
+//-------------------------------
+
+// console.log(age);
+
+// let age=21;      // declaration & initialization
+// console.log(age);
+
+// let age=42;
+// console.log(age);
+
+// age=23;           // re-assignment
+// console.log(age);
+//--------------------------------
+
+// console.log(age);
+
+// comst age=21;      // declaration & initialization
+// console.log(age);
+
+// const age=42;
+// console.log(age);
+
+// age=23;           // re-assignment
+// console.log(age)
+//--------------------------------
+
+// Arithmetic Operations in JavaScript
+
+// Arithmetic Addition
+let op=1+1;
+console.log(op);
+op=1+'a';
+console.log(op); 
+op='a'+1;
+console.log(op); 
+op='1'+'a';
+console.log(op);
+
+op=1+1;
+console.log(op);
+op=1+'1';
+console.log(op); 
+op='1'+1;
+console.log(op); 
+op='1'+'1';
+console.log(op); 
+
+// Arithmetic Subtraction
+op=1-1;
+console.log(op);
+op=1-'a';
+console.log(op); 
+op='a'-1;
+console.log(op); 
+op='1'-'a';
+console.log(op);
+
+op=1-1;
+console.log(op);
+op=1-'10';
+console.log(op); 
+op='11'-1;
+console.log(op); 
+op='10'-'14';
+console.log(op);
+
+// Arithmetic Multiplication
+op=1*1;
+console.log(op);
+op=1*'a';
+console.log(op); 
+op='a'*1;
+console.log(op); 
+op='1'*'a';
+console.log(op); 
+
+op=1*1;
+console.log(op);
+op=1*'11';
+console.log(op); 
+op='10'*11;
+console.log(op); 
+op='10'*'14';
+console.log(op); 
+
+// Arithmetic Division
+op=1/1;
+console.log(op);
+op=1/'a';
+console.log(op); 
+op='a'/1;
+console.log(op); 
+op='1'/'a';
+console.log(op); 
+
+op=1/1;
+console.log(op);
+op=1/'10';
+console.log(op); 
+op='100'/10;
+console.log(op); 
+op='10'/'5';
+console.log(op);
+ 
