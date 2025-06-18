3b79454c6b6158ac7405f28d4d1c68e7c771727b
Priyank Singh <priyanksingh5004@gmail.com>
2025-06-18 10:26:27 +0530

    commit

---
+++ Day3/script.js
@@ -0,0 +1,35 @@
+console.log("Hello");
+
+// Iterating over an array using different loops
+//const arr=["cat","dog","elephant","tiger"];
+//-----------------------------------------------
+
+// for loop
+// for (let i=0;i<arr.length;i++){
+//     const elem=arr[i];
+//     console.log(elem,i);
+// }
+//-----------------------------------------------
+
+// for-in loop
+// for(let i in arr){
+//     //all the keys one-by-one (avoid)
+//     const elem=arr[i];
+//     console.log(elem,i);
+// }
+//-----------------------------------------------
+
+// for-of loop
+// for(let i of arr){
+//     //all the values one-by-one (avoid)
+//     console.log(i);
+// }
+//-----------------------------------------------
+
+// for-Each loop
+// const myfun=()=>{
+//     console.log("Hello!");
+// }
+
+// arr.forEach(myfun);
+//-----------------------------------------------
